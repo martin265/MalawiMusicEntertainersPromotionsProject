288c35eb1cd2042002d48654054a74b2d4692e20
martin265 <martinsilungwe12@gmail.com>
2024-03-08 10:13:20 +0200

    Working on the routing part of the project

--- main.py
+++ main.py
@@ -2,7 +2,28 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.bgcolor = "#fafafa"
+    page.window_center()
+    page.window_min_width = 768
+
+    # ============= the function for the routing will be here ======== //
+    def router(route):
+        try:
+            page.views.clear() # === removing the fault bahaviour
+        except Exception as ex:
+            page.snack_bar = ft.SnackBar(
+                content=ft.Row(
+                    controls=[
+                        ft.Text(
+                            "something went wrong at {}".format(ex)
+                        )
+                    ]
+                )
+            )
+            page.snack_bar.open = True
+            page.update()
+    page.update()
 
 
-ft.app(main)
+if __name__ == "__main__":
+    ft.app(target=main, assets_dir="assets")
