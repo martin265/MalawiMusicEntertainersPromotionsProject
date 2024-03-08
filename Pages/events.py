10d7ddcd729e3c2ef4ea689a0c81b17ee5e93ec9
martin265 <martinsilungwe12@gmail.com>
2024-03-08 12:28:06 +0200

    Working on the events page of the project

--- Pages/events.py
+++ Pages/events.py
@@ -7,30 +7,51 @@ class EventsPage(ft.Container):
         self.page = page
 
         # ========= the user controls will be here ====== //
-        self.controls = [
-            ft.SafeArea(
-                content=ft.Container(
-                    content=ft.Column(
-                        controls=[
-                            #  ========= responsive row for the containers
-                            ft.ResponsiveRow(
-                                [
+        self.content = ft.Container(
+            content=ft.SafeArea(
+                content=ft.ResponsiveRow(
+                    #  ======== the first container for the events details will be here ========= //
+                    [
+                        ft.Container(
+                            content=ft.Column(
+                                controls=[
+                                    # ======== the container for the top information
                                     ft.Container(
-                                        bgcolor="blue",
-                                        height=200,
-                                        content=ft.Text("hello"),
-                                        col={"sm": 12, "md": 12, "lg": 9}
+                                        content=ft.Column(
+                                            controls=[
+                                                ft.Row(
+                                                    controls=[
+                                                        ft.Icon(
+                                                            ft.icons.EVENT_NOTE_ROUNDED,
+                                                            size=60,
+                                                            color="#212121"
+                                                        )
+                                                    ]
+                                                )
+                                            ]
+                                        )
                                     ),
-                                    ft.Container(
-                                        bgcolor="black",
-                                        height=200,
-                                        content=ft.Text("hello"),
-                                        col={"sm": 12, "md": 12, "lg": 3}
-                                    )
                                 ]
-                            )
-                        ]
-                    )
+                            ),
+                            gradient=ft.LinearGradient(
+                                colors=[
+                                    "#CBCCFF",
+                                    "#CBCCFF"
+                                ],
+                                begin=ft.alignment.center_left,
+                                end=ft.alignment.top_right
+                            ),
+                            border_radius=ft.border_radius.all(10),
+                            col={"sm": 12, "md": 9, "lg": 9}
+                        ),
+
+                        ft.Container(
+                            content=ft.Text("hello"),
+                            height=300,
+                            bgcolor="yellow",
+                            col={"sm": 12, "md": 3, "lg": 3}
+                        ),
+                    ]
                 )
             )
-        ]
+        )
