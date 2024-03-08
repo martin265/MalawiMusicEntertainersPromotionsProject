e5ad8f2ef9956882b8962225f8d5c102438b04bb
martin265 <martinsilungwe12@gmail.com>
2024-03-08 12:47:08 +0200

    Working on the events page of the project

--- Pages/events.py
+++ Pages/events.py
@@ -5,6 +5,12 @@ class EventsPage(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
+        #  ======== the fonts for the system will be here ======= //
+        self.page.fonts = {
+            "manrope": "assets/fonts/Manrope/static/Manrope-Light.ttf",
+            "manrope-sem-bold": "assets/fonts/Manrope/static/Manrope-Regular.ttf",
+            "manrop-bold": "assets/fonts/Manrope/static/Manrope-Bold.ttf"
+        }
 
         # ========= the user controls will be here ====== //
         self.content = ft.Container(
@@ -17,9 +23,11 @@ class EventsPage(ft.Container):
                                 controls=[
                                     # ======== the container for the top information
                                     ft.Container(
+                                        margin=ft.margin.only(top=40),
                                         content=ft.Column(
                                             controls=[
                                                 ft.Row(
+                                                    alignment=ft.MainAxisAlignment.CENTER,
                                                     controls=[
                                                         ft.Icon(
                                                             ft.icons.EVENT_NOTE_ROUNDED,
@@ -27,6 +35,21 @@ class EventsPage(ft.Container):
                                                             color="#212121"
                                                         )
                                                     ]
+                                                ),
+                                                # ====== container for the text ======== //
+                                                ft.Container(
+                                                    content=ft.Row(
+                                                        alignment=ft.MainAxisAlignment.CENTER,
+                                                        controls=[
+                                                            ft.Text(
+                                                                "add event details".title(),
+                                                                size=30,
+                                                                style=ft.TextStyle(
+                                                                    font_family="manrop-bold"
+                                                                )
+                                                            )
+                                                        ]
+                                                    )
                                                 )
                                             ]
                                         )
