c7730980cdd982cb20abe1c182df77febe74bb7d
martin265 <martinsilungwe12@gmail.com>
2024-03-18 18:29:12 +0200

    Working on the artists page

--- Pages/artists.py
+++ Pages/artists.py
@@ -35,7 +35,9 @@ class Artists(ft.Container):
                                     ft.Stack(
                                         controls=[
                                             ft.Image(
-                                                src="https://images.pexels.com/photos/755416/pexels-photo-755416.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                                src="https://images.pexels.com/photos/755416/pexels-photo-755416.jpeg"
+                                                    "?auto=compress&cs=tinysrgb&w=600",
+                                                fit=ft.ImageFit.FIT_WIDTH
                                             )
                                         ]
                                     )
