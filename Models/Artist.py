50505c9f0d531e294873ea5863419cb72b12d4f4
martin265 <martinsilungwe12@gmail.com>
2024-03-21 10:18:32 +0200

    Writing functions to validate the records

---
+++ Models/Artist.py
@@ -0,0 +1,23 @@
+import os
+from Config.config import supabase
+
+
+class Artist:
+    def __init__(self, first_name, last_name, email, phone_number, gender,
+                 age, genre, residence, artist_biography):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.gender = gender
+        self.age = age
+        self.genre = genre
+        self.residence = residence
+        self.artist_biography = artist_biography
+
+
+    def save_artist_records_func(self):
+        try:
+        except Exception as ex:
+            print("something went wrong at {}".format(ex))
