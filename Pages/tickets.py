aa893dddfb100a2b6a121af58f1eaebe39418a1d
martin265 <martinsilungwe12@gmail.com>
2024-03-22 15:44:26 +0200

    working on the tickets page

--- Pages/tickets.py
+++ Pages/tickets.py
@@ -5,3 +5,32 @@ class Tickets(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
+        #  ========= the content for the tickets page will be here ==== //
+        self.content = ft.Column(
+            controls=[
+                ft.SafeArea(
+                    content=ft.ResponsiveRow(
+                        controls=[
+                            #  ========== the container for the form here
+                            ft.Container(
+                                content=ft.Column(
+                                    controls=[
+
+                                    ]
+                                ),
+                                col={"sm": 12, "md": 9, "lg": 8}
+                            ),
+                            #  =========== the container for the other tools
+                            ft.Container(
+                                content=ft.Column(
+                                    controls=[
+
+                                    ]
+                                ),
+                                col={"sm": 12, "md": 9, "lg": 4}
+                            )
+                        ]
+                    )
+                )
+            ]
+        )
