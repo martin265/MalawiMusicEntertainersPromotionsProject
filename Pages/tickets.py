588090776e7e9e5a3db7ca292c49d25704d90276
martin265 <martinsilungwe12@gmail.com>
2024-03-31 10:39:11 +0200

    working on the artists page

--- Pages/tickets.py
+++ Pages/tickets.py
@@ -5,6 +5,7 @@ class TicketsPage(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
+        self.page.adaptive = True
         #  ======== the fonts for the system will be here ======= //
         self.page.scroll = ft.ScrollMode.HIDDEN
         self.page.fonts = {
@@ -17,6 +18,20 @@ class TicketsPage(ft.Container):
             height=self.page.height,
             scroll=ft.ScrollMode.ADAPTIVE,
             controls=[
-
+                ft.ResponsiveRow(
+                    controls=[
+                        ft.Container(
+                            gradient=ft.LinearGradient(
+                                colors=[
+                                    "#CBCCFF",
+                                    "#CBCCFF"
+                                ],
+                                begin=ft.alignment.top_left,
+                                end=ft.alignment.bottom_center
+                            ),
+                            col={"sm": 12, "md": 12, "lg": 12}
+                        )
+                    ]
+                )
             ]
         )
