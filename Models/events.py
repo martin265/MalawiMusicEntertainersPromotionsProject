2babb8d28b6b55021c10940ec749831bba6b15b9
martin265 <martinsilungwe12@gmail.com>
2024-03-31 13:11:18 +0200

    working on the tickets page

---
+++ Models/events.py
@@ -0,0 +1,24 @@
+import flet as ft
+
+
+class Events:
+    def __init__(self, ticket_type, ticket_price, event_name, event_type,
+                 event_capacity, event_organizers, special_offers, accessibility_information,
+                 age_restrictions, event_description, payment_methods, event_category,
+                 artists_name):
+        super().__init__()
+        self.ticket_name = ticket_type
+        self.ticket_price = ticket_price
+        self.event_name = event_name
+        self.event_type = event_type
+        self.event_capacity = event_capacity
+        self.event_organizers = event_organizers
+        self.special_offers = special_offers
+        self.accessibility_information = accessibility_information
+        self.age_restrictions = age_restrictions
+        self.event_description = event_description
+        self.payment_methods = payment_methods
+        self.event_category = event_category
+        self.artists_name = artists_name
+
+    def save_events_details_fun(self):
