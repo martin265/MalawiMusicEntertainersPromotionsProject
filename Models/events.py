20b04f0694b1a4e49bcdcfdf7439cbd573d07510
martin265 <martinsilungwe12@gmail.com>
2024-03-31 13:18:14 +0200

    working on the tickets page

--- Models/events.py
+++ Models/events.py
@@ -1,5 +1,6 @@
 import flet as ft
-import supabase
+from Config.config import supabase
+
 
 class Events:
     def __init__(self, ticket_type, ticket_price, event_name, event_type,
@@ -7,7 +8,7 @@ class Events:
                  age_restrictions, event_description, payment_methods, event_category,
                  artists_name):
         super().__init__()
-        self.ticket_name = ticket_type
+        self.ticket_type = ticket_type
         self.ticket_price = ticket_price
         self.event_name = event_name
         self.event_type = event_type
@@ -22,7 +23,23 @@ class Events:
         self.artists_name = artists_name
 
     def save_events_details_fun(self):
+        """the function will save the records into the database"""
         try:
-
+            data, count = supabase.table("Events").insert(
+                {
+                    "ticket_type": self.ticket_type,
+                    "ticket_price": self.ticket_price,
+                    "event_name": self.event_name,
+                    "event_capacity": self.event_capacity,
+                    "event_organizers": self.event_organizers,
+                    "special_offers": self.special_offers,
+                    "accessibility_information": self.accessibility_information,
+                    "age_restriction": self.age_restrictions,
+                    "event_description": self.event_description,
+                    "payment_method": self.payment_methods,
+                    "event_category": self.event_category,
+                    "artists_name": self.artists_name
+                }
+            ).execute()
         except Exception as ex:
             print(ex)
